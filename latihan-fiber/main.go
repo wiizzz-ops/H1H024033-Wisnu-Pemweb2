6ade61d96fd502500d361b25e973d2b14c8d2e52
Wisnu Satya <wisnusatya409@gmail.com>
2026-09-08 20:06:23 +0700

    menambahkan 1 endpoint GET

--- latihan-fiber/main.go
+++ latihan-fiber/main.go
@@ -6,19 +6,28 @@ import (
 	"github.com/gofiber/fiber/v3"
 )
 
-func main () {
+func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c fiber.Ctx) error {
-		return c.SendString("IWAK TEMPIK!!")
+		return c.SendString("Halo, World!")
 	})
 
 	app.Get("/api/info", func(c fiber.Ctx) error {
 		return c.JSON(fiber.Map{
-			"aplikasi": "IWAK TEMPIK!!",
+			"aplikasi": "Halo",
 			"versi":    "1.0.0",
 			"status":   "berjalan",
 		})
 	})
+
+	app.Get("/api/mahasiswa", func(c fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"nim":           "H1H024033",
+			"nama":          "Wisnu Satya Herlambang",
+			"program_studi": "Teknik Komputer",
+		})
+	})
+
 	log.Fatal(app.Listen(":3000"))
 }
